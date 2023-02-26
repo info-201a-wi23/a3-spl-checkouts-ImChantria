e8abc09af17c53f0fbce45e91abc9911d8ebc1f9
ImChantria <chantria@uw.edu>
2023-02-25 18:14:01 -0800

    bar graph for last chart

--- chart2_example.R
+++ chart2_example.R
@@ -1,6 +1,36 @@
-x_values <- seq(1, 3)
-y_values <- seq(1,3)
+#Bar plot for A3
 
-library(ggplot2)
-ggplot() +
-  geom_line(aes(x=x_values, y = y_values))
+#Load libraries
+library("ggplot2")
+library("plotly")
+library("dplyr")
+library("tidyr")
+
+#Load data
+spl_df <- read.csv("Cain_Checkouts_by_Title.csv", stringsAsFactors = FALSE)
+
+author_df <- spl_df %>% 
+  filter(str_detect(Creator, "Susan")) %>% 
+  filter(str_detect(Creator, "Cain")) 
+
+
+diff_books <- author_df %>% 
+  group_by(Title) %>% 
+  summarize(count = n())
+
+#Combine data between two books
+combined_books <- diff_books %>%
+  mutate(Book = ifelse(str_detect(Title, "Quiet"), "Quiet",
+                       ifelse(str_detect(Title, "Bittersweet"), "Bittersweet", NA))) %>%
+  drop_na(Book) %>%
+  group_by(Book) %>%
+  summarize(count = sum(count))
+
+#Plot
+colors <- c("royalblue", "maroon")
+
+ggplot(combined_books, aes(x = Book, y = count, fill = Book)) +
+  geom_bar(stat = "identity") +
+  scale_fill_manual(values = colors) +
+  labs(x = "Book", y = "Number of Checkouts", 
+       title = "Checkout Count for Susan Cain's Books from January 2012 to January 2023")
